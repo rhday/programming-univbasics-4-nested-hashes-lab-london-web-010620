a120a3eadd66bea3d808f088a605dda836079544
Ruari Day <ruariday@gmail.com>
2019-12-03 13:45:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-  alan_kay_is_known_for = 
+  alan_kay_is_known_for = alan_kay[:known_for]
 end
 
 def dennis_ritchies_language
