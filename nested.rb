9c6f8dc15bbb88e8ec0420b22a508776b4ab3d55
Ruari Day <ruariday@gmail.com>
2019-12-03 13:53:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -54,7 +54,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages] = "C"
+     
 end
 
 def adding_matz
